785cb90d53c8eac2dccaacdc60504a97bc3b5c07
EQL2020 <eqlrepository@gmail.com>
2020-11-02 12:32:15 +0100

    Ajout ValRef + final

---
+++ JavaPOO/src/fr/eql/ai108/valref/Personne.java
@@ -0,0 +1,38 @@
+package fr.eql.ai108.valref;
+
+public class Personne {
+	
+	private String nom;
+	private String prenom;
+	private int age;
+	
+	public Personne() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Personne(String nom, String prenom, int age) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}	
+}
