a0edfb750c06e844bad91d39d2f9d4c9d46de38d
EQL2020 <eqlrepository@gmail.com>
2020-11-09 11:46:37 +0100

    Ajout IP (fin)

--- JavaPOO/src/fr/eql/ai108/io/TestWriter.java
+++ JavaPOO/src/fr/eql/ai108/io/TestWriter.java
@@ -1,5 +1,6 @@
 package fr.eql.ai108.io;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -18,6 +19,28 @@ public class TestWriter {
 		}finally {
 			try {
 				fw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		FileWriter out = null;
+		BufferedWriter bw = null;
+		try {
+			//De façon générale, le BufferedWriter fait la même chose que le 
+			//FileWriter mais est plus performant et dispose de plus de méthodes utilitaires
+			out = new FileWriter("c:/DossierAI108/testBW.txt", true);
+			bw = new BufferedWriter(out);
+			bw.write("Hello AI108");
+			bw.newLine();
+			bw.write(42);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				bw.close();
+				out.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
